990754f6deaf17ee961992562d52a1a2e5f3d4ab
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-10 15:01:35 +0530

    file added

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
+let data = {
+  name: "harkirat",
+};
+console.log("server is up");
 app.get("/", (req, res) => {
-  res.send("hello world");
+  console.log("hi there");
+  res.send("<h1>This is a WebPage</h1>");
 });
+
+app.get("/dashboard", (req, res) => {
+  res.send("<h2>this ia dashboard</h2>");
+});
+app.get("/api/data", (req, res) => {
+  res.send(data);
+});
+
 app.listen(PORT, () => console.log(`Server is Started At: ${PORT}`));
