ceecc79066140f0a411e9ad8c2a53fa4cbe86f3f
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-12 11:07:33 +0530

    some file added moved

--- project/src/server.js
+++ project/src/server.js
@@ -1,1 +1,9 @@
-console.log("hi there");
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send(" hi there");
+});
+
+app.listen(PORT, () => console.log(`Server Started at:${PORT}`));
