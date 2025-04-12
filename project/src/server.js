bd17f7a62e54fbddc69f7a16e2840cce27c38ada
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-12 11:29:26 +0530

    question solved

--- project/src/server.js
+++ project/src/server.js
@@ -1,9 +1,19 @@
 import express from "express";
-
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//Get the file path from the URL of current module
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name from the file path
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+//Serving up the HTML file from th public directory
 app.get("/", (req, res) => {
-  res.send(" hi there");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server Started at:${PORT}`));
