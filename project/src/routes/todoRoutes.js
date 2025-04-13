195fbb1817c386de5cbbf9f015f720a5b97ac4ee
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-13 20:10:44 +0530

    file updated to routes

--- project/src/routes/todoRoutes.js
+++ project/src/routes/todoRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 //Get all todos
 router.get("/", (req, res) => {
   const getTodos = db.prepare("SELECT * FROM todos WHERE user_id =?");
-  const todos = getTodos.all(req.userID);
+  const todos = getTodos.all(req.userId);
   res.json(todos);
 });
 
