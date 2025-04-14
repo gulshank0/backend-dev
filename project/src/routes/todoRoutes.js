63b289595622e747ef41743de7edd0a2843e9123
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-14 12:25:44 +0530

    file updated to routes

--- project/src/routes/todoRoutes.js
+++ project/src/routes/todoRoutes.js
@@ -11,10 +11,23 @@ router.get("/", (req, res) => {
 });
 
 //Create a  todos
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const { task } = req.body;
+  const insertTodo = db.prepare(
+    `INSERT INTO todos(user_id,task) VALUES (?,?) `,
+  );
+  insertTodo.run(req.userId, task);
+  res.json({ id: insertTodo.lastId, task, completed: 0 });
+});
 // Update a Todos
-router.put("/:id", (req, res) => {});
-
+router.put("/:id", (req, res) => {
+  const { completed } = req.body;
+  const { id } = req.params;
+  const { page } = req.query;
+  const updateTodo = db.prepare("UPDATE todos SET completed=? WHERE id=?");
+});
+updateTodo.run(completed, id);
+res.json({ messege: "Todo completed" });
 //Delete a Todos
 router.delete("/:id", (req, res) => {});
 
